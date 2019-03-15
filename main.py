f206f494a0cde6df528b115bd83a907e1e98a8bb
SJ <siddharthjani@hotmail.com>
2019-03-15 20:56:47 +0000

    loading of data set and pre processing

---
+++ main.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, division, print_function
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
